12106c3a2919c06de7352e4247ad2400aaeba597
Ranka Jovanovic <ranka.j.94@gmail.com>
2021-10-31 14:54:58 +0100

    added database, seeder, factory

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'name' => $this->faker->word(),
+            'description'=> $this->faker->text($maxNbChars = 200),
+            'price'=> $this->faker->numberBetween($min = 1000, $max = 90000),
+            'payment'=> $this->faker->randomElement($array = array ('card','cash')) ,
+            'delivery'=> $this->faker->randomElement($array = array ('regular','fast')),
+            'image'=> $this->faker->image('public/storage/images',640,480, null, false)
+        ];
+    }
+}
