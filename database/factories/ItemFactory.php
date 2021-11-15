854adc2b9c8b7b737d458a4d59715168640d3b04
Ranka Jovanovic <ranka.j.94@gmail.com>
2021-11-15 03:55:51 +0100

    update item factory

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\User;
+use Carbon\Carbon;
+
 class ItemFactory extends Factory
 {
     /**
@@ -17,10 +19,11 @@ class ItemFactory extends Factory
             'user_id' => User::all()->random()->id,
             'name' => $this->faker->word(),
             'description'=> $this->faker->text($maxNbChars = 200),
-            'price'=> $this->faker->numberBetween($min = 1000, $max = 90000),
+            'price'=> $this->faker->numberBetween($min = 200, $max = 20000),
             'payment'=> $this->faker->randomElement($array = array ('Card','Cash')) ,
             'delivery'=> $this->faker->randomElement($array = array ('Regular','Fast')),
-            'image'=> $this->faker->image('public/storage/images',640,480, null, false)
+            'image'=> 'https://placeimg.com/500/500/tech?' . rand(1, 100),
+            'end_time' => Carbon::now()->addDays(10),
         ];
     }
 }
